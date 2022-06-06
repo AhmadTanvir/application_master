61c93bb15cb06236cb6ee5bd16a51e672ca2b5fd
AhmadTanvir <atasiddiq@gmail.com>
2022-06-06 21:46:51 +0600

    Initial commit

---
+++ database/migrations/2022_06_04_064559_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('menu_name');
+            $table->integer('parent_id');
+            $table->string('action_name');
+            $table->string('route_name');
+            $table->integer('menu_order');
+            $table->integer('insert_by');
+            $table->integer('update_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
