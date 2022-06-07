8b9817910c3e74a0483a426183dd9df273575791
AhmadTanvir <atasiddiq@gmail.com>
2022-06-07 07:35:04 +0600

    Message Sending Through Gmail

---
+++ resources/views/emails/MailSending.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+    <h2>আস্সালামু আলাইকুম {{$body['name']}}, ভাই</h2>
+
+    মেইল পেলে জানাবেন
+
+
+    Happy coding!
+
+    জাজাকাল্লাহু খাইরান,
+    তানভীর আহমাদ.
+@endcomponent
